c1376807e94fc2265631830e6b5db97352ae10c3
josebb159 <jose.betancourtbruzual@gmail.com>
2024-05-05 14:12:31 -0500

    test

--- view/dinamic/tienda/producto.php
+++ view/dinamic/tienda/producto.php
@@ -28,6 +28,8 @@
 
 <?php
 echo $_SESSION['id_tienda'];
+
+echo "aa";
 ?>
 
 						<table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
