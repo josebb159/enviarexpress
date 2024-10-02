c8c121d00e418149fddb7a1d14dc2cd553cab261
josebb159 <jose.betancourtbruzual@gmail.com>
2024-10-01 21:28:15 -0500

    fix code

--- model/api.php
+++ model/api.php
@@ -1444,7 +1444,7 @@ public function obtener_domiciliario($uid){
         $conexion = new Conexion();
      
 
-        $sql = "SELECT orden.*, 0 as distancia, direccion.telefono as tele_direccion, direccion.latitude as latitude_direccion, direccion.longitude as longitude_direccion, (select usuarios.nombre from usuarios where usuarios.id=orden.id_cliente) as cliente FROM orden left join direccion on direccion.id_direccion=orden.id_entrega left join usuarios on usuarios.id=orden.id_domiciliario where orden.status_orden_envio<5 and usuarios.uid='".$uid."'";
+        $sql = "SELECT orden.* ,(SELECT comercios.nombre from comercios WHERE comercios.id_user=orden.id_tienda) as tienda,  0 as distancia, direccion.telefono as tele_direccion, (SELECT comercios.latitude from comercios WHERE comercios.id_user=orden.id_tienda)  as latitude, (SELECT comercios.longitude from comercios WHERE comercios.id_user=orden.id_tienda) as longitude, (select usuarios.nombre from usuarios where usuarios.id=orden.id_cliente) as cliente FROM orden left join direccion on direccion.id_direccion=orden.id_entrega left join usuarios on usuarios.id=orden.id_domiciliario where orden.status_orden_envio<5 and usuarios.uid='".$uid."' order by orden_list ASC limit 1";
       // $sql = "SELECT  * FROM usuarios where usuario='jose' and contrasena='123' and estado=1";
         $reg = $conexion->prepare($sql);
 
