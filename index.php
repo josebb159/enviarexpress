d92049c7abaa2446683c3dfa7e09fd62334935f5
jose.betancourtbruzual <jose.betancourtbruzual@gmail.com>
2025-02-16 12:29:37 -0500

    fix code 2

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="Themesdesign" name="author" />
-        <!-- App favicon -->
+        <!-- App favicon-->
         <link rel="shortcut icon" href="assets/images/favicon.ico">
 
         <!-- Bootstrap Css -->
